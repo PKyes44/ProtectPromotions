1bb462014fb4053e0c7f7170754111b7a05c21df
PKyes44 <rugate23@gmail.com>
2024-04-09 15:32:40 +0900

    20240409-01

--- app/src/main/java/com/atifqamar/smsdefaulthandler/SMSActivity.kt
+++ app/src/main/java/com/atifqamar/smsdefaulthandler/SMSActivity.kt
@@ -1,19 +1,25 @@
 package com.atifqamar.smsdefaulthandler;
 
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.atifqamar.smsdefaulthandler.SmsRecycler.MyRecyclerAdapter
 import com.atifqamar.smsdefaulthandler.SmsRecycler.SMSItem
+import com.atifqamar.smsdefaulthandler.databinding.ActivityMainBinding
+import com.atifqamar.smsdefaulthandler.sqlite.ChatPartner
+import com.atifqamar.smsdefaulthandler.sqlite.DBHelper
 
 
 class SMSActivity : AppCompatActivity() {
+        lateinit var db: DBHelper
+        var chatPartners = ArrayList<ChatPartner>()
+        private lateinit var binding: ActivityMainBinding
 
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
-                setContentView(R.layout.activity_sms)
+                setContentView(R.layout.activity_main)
 
                 var mRecyclerView = findViewById<RecyclerView>(R.id.sms_recycler_view)
 
@@ -25,24 +31,49 @@ class SMSActivity : AppCompatActivity() {
                 mRecyclerView.setLayoutManager(LinearLayoutManager(this))
 
                 /* adapt data */
-                var mfriendItems : ArrayList<SMSItem> = ArrayList()
+                var mfriendItems: ArrayList<SMSItem> = ArrayList()
                 for (i in 1..10) {
                         if (i % 2 == 0) mfriendItems.add(
                                 SMSItem(
 //                                        R.drawable.ic_woman,
                                         i.toString() + "번째 사람",
-                                        "[ 최근에 보낸 메시지 ]"
+                                        i.toString() + "번째 상태메시지"
                                 )
                         ) else mfriendItems.add(
                                 SMSItem(
 //                                        R.drawable.ic_man,
                                         i.toString() + "번째 사람",
-                                        "[ 최근에 보낸 메시지 ]"
+                                        i.toString() + "번째 상태메시지"
                                 )
                         )
                 }
                 mRecyclerAdapter.setSMSList(mfriendItems)
+//
+                var mSMSItems: ArrayList<SMSItem> = ArrayList()
 
+                binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                db = DBHelper(this)
+
+                for (i in 1..10) {
+                        db.addPartner(
+                                ChatPartner(
+                                        i.toString(),
+                                        i.toString() + "번째 상대",
+                                )
+                        )
+                }
+                chatPartners.clear()
+                chatPartners = db.allChatPartners as ArrayList<ChatPartner>
+                Log.d("SMSItems", chatPartners.get(0).name)
+
+                var i = 0
+                for (chatPartner in chatPartners) {
+                        mSMSItems.add(SMSItem(chatPartner.name, i.toString()))
+                        i += 1
+                }
+                mRecyclerAdapter.setSMSList(mSMSItems)
         }
 }
 
--- app/src/main/java/com/atifqamar/smsdefaulthandler/sqlite/DBHelper.kt
+++ app/src/main/java/com/atifqamar/smsdefaulthandler/sqlite/DBHelper.kt
@@ -12,34 +12,34 @@ class DBHelper(
         ) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
         companion object{
         const val DATABASE_VERSION = 1
-        const val DATABASE_NAME = "USER_AUTH"
+        const val DATABASE_NAME = "SMS"
 
         // Table
-        const val TABLE_NAME = "USER"
+        const val TABLE_NAME = "CHAT_PARTNER"
         const val UID = "UID"
         const val COL_ID = "ID"
-        const val COL_PW = "PW"
+//        const val COL_PW = "PW"
         const val COL_NAME = "NAME"
         }
 
         override fun onCreate(db: SQLiteDatabase) {
         // USER 라는 이름의 테이블을 생성하고 column 은 ID, PW, NAME 3개를 생성했습니다.
         // UID 는 SQLite 를 사용하기 위해서 필수적으로 필요한 column
-        var sql: String = "CREATE TABLE IF NOT EXISTS " +
-        "$TABLE_NAME ($UID integer primary key autoincrement, " +
-        "$COL_ID text, $COL_PW text, $COL_NAME text);"
-        db.execSQL(sql)
+                var sql: String = "CREATE TABLE IF NOT EXISTS " +
+                "$TABLE_NAME ($UID integer primary key autoincrement, " +
+                "$COL_ID text, $COL_NAME text);"
+                db.execSQL(sql)
         }
 
         override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        val sql = "DROP TABLE IF EXISTS $TABLE_NAME"
+                val sql = "DROP TABLE IF EXISTS $TABLE_NAME"
 
-        db.execSQL(sql)
-        onCreate(db)
+                db.execSQL(sql)
+                onCreate(db)
         }
 
         // allUsers 리스트에 getter 를 만들어서 db 에 저장되어있는 모든 유저 정보를 가져옵니다.
-        val allUsers:List<ChatPartner>
+        val allChatPartners:List<ChatPartner>
 @SuppressLint("Range")
         get() {
                 val chatPartners = ArrayList<ChatPartner>()
@@ -61,10 +61,10 @@ class DBHelper(
         }
         // db 에 새로운 유저를 추가하는 메소드
         fun addPartner(chatPartner: ChatPartner){
-                if(checkIdExist(chatPartner.id)) {
-                        Toast.makeText(this.context, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show()
-                        return
-                }
+//                if(checkIdExist(chatPartner.id)) {
+//                        Toast.makeText(this.context, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show()
+//                        return
+//                }
                 val db = this.writableDatabase
                 val values = ContentValues()
                 values.put(COL_ID, chatPartner.id)
@@ -93,4 +93,4 @@ class DBHelper(
         db.delete(TABLE_NAME, "$COL_ID=?", arrayOf(chatPartner.id))
         db.close()
         }
-        }
+}
